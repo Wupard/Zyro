0af79520a133fde40208495857f5870b9e52ceae
Wupard <yusufkahraman023@gmail.com>
2026-06-17 00:00:36 +0300

    Favicon icin sifir bosluklu (zyro_logo_cropped.png) secilerek favicon boyutu duzeltildi, site ici logo ve yazi arasindaki devasa bosluk negatif margin ile kapatildi

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Bu dosya SADECE cache ve offline desteği içindir.
 // ============================================================
 
-const CACHE_NAME = 'zyro-v8';
+const CACHE_NAME = 'zyro-v9';
 const ASSETS = [
   '/',
   '/index.html',
